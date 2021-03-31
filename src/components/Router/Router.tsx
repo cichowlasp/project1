006b272969ee24bd0696306e51b794ccbb128ea2
Piotr Cichowlas <p.cichowlas@tkhtechnology.com>
2021-03-31 19:14:21 +0200

    implemented router

---
+++ src/components/Router/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+export default function Router() {
+    return (
+        <BrowserRouter>
+                <Switch>
+                    <Route path='/'>
+                        <Home />
+                    </Route>
+                </Switch>
+        </BrowserRouter>
+    );
+}
